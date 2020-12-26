9e33a29ee0064475aa67ef830dcf0636e69978e4
fadiHB <eng.fadi.hb@gmail.com>
2020-12-26 19:09:24 +0200

    second part

--- automation/automation.py
+++ automation/automation.py
@@ -5,13 +5,14 @@ def get_text (file_path):
         text = f.read()
         return text
 
-def extractEmails( text ):
+def extract_emails( text ):
     mail_pattern = r'[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+'
-    all_mails_in_text =  re.findall( mail_pattern ,text )
+    all_mails_in_text =  re.findall( mail_pattern, text )
     emails = []
     for mail in all_mails_in_text:
         if mail not in emails:
             emails.append(mail)
+    emails.sort()
 
     with open('emails.txt', 'w+') as f:
         index = 1
@@ -20,8 +21,22 @@ def extractEmails( text ):
             index += 1
     return
       
-
-
+def extract_phone_number(text):
+    #phone_number_pattern = r'((\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?.?))((\d{3}(\s|-?.?))|(\d{2}(\s|-?)))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?)'
+    #phone_number_pattern = r'(((\(\d{3}\)\s?)|(\d{3})(\s|-?.?))?(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?.?))((\d{3}(\s|-?.?))|(\d{2}(\s|-?)))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?)'
+    phone_number_pattern = r'(((\(\d{3}\)\s?)|(\+\d{1})(\s|-?.?))?((\(\d{3}\)\s?)|(\d{3})(\s|-?.?))?(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?.?))((\d{3}(\s|-?.?))|(\d{2}(\s|-?)))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?)'
+    all_numbers_in_text = re.findall(phone_number_pattern, text)
+    phone_numbers = []
+    for number in all_numbers_in_text:
+        if number[0] not in phone_numbers:
+            phone_numbers.append(number[0])
+    phone_numbers.sort()
+    with open('phone_numbers.txt', 'w+') as f:
+        index = 1
+        for i in phone_numbers:
+            f.write( 'phone number ' + str(index) + ':\t\t' + i + '\n')
+            index += 1
+    return
 
 
     
@@ -29,5 +44,6 @@ def extractEmails( text ):
 if __name__ == "__main__":
     file_path = 'automation/assets/potential-contacts.txt'
     text = get_text(file_path)
-    extractEmails(text)
+    extract_emails(text)
+    extract_phone_number(text)
 
